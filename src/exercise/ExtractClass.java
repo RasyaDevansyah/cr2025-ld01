5055c08837c881b2cb726a80967f8707c76d7413
Rasya Devansyah <rasya.devan21@gmail.com>
2025-02-13 16:49:28 +0700

    Extract Class Done

--- src/exercise/ExtractClass.java
+++ src/exercise/ExtractClass.java
@@ -1,28 +1,46 @@
 package exercise;
 public class ExtractClass {
 // todo extract class TelephoneNumber from class Person
+	
+//DONE
 	class Person {
 	  private String name;
-	  private String officeAreaCode;
-	  private String officeNumber;
+	  private TelephoneNumber telephoneNumber;
 
 	  public String getName() {
 		return name;
 	  }
-	  public String getTelephoneNumber() {
-		return ("(" + officeAreaCode + ") " + officeNumber);
+
+	  public TelephoneNumber getTelephoneNumber() {
+		  return telephoneNumber;
 	  }
-	  public String getOfficeAreaCode() {
-		return officeAreaCode;
-	  }
-	  public void setOfficeAreaCode(String arg) {
-		officeAreaCode = arg;
-	  }
-	  public String getOfficeNumber() {
-		return officeNumber;
-	  }
-	  public void setOfficeNumber(String arg) {
-		officeNumber = arg;
-	  }
+	  
 	}
+	
+	
+	class TelephoneNumber{
+		private String officeAreaCode;
+		private String officeNumber;
+		
+		
+		public String getTelephoneNumber() {
+			return ("(" + officeAreaCode + ") " + officeNumber);
+		}
+		
+		public String getOfficeAreaCode() {
+			return officeAreaCode;
+		}
+		public void setOfficeAreaCode(String arg) {
+			officeAreaCode = arg;
+		}
+		public String getOfficeNumber() {
+			return officeNumber;
+		}
+		public void setOfficeNumber(String arg) {
+			officeNumber = arg;
+		}
+		
+		
+	}
+	
 }
