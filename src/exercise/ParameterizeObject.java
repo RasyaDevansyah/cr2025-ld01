9603e2be570a5d083daac8437b8fe6b837b23e22
Rasya Devansyah <rasya.devan21@gmail.com>
2025-02-21 19:27:00 +0700

    Parameterize Object Done

--- src/exercise/ParameterizeObject.java
+++ src/exercise/ParameterizeObject.java
@@ -5,52 +5,64 @@ import java.util.Vector;
 
 
 public class ParameterizeObject {
-	// TODO: change the logic of Date start & Date end of this Account class by
-	//       introduce parameter object
+	// change the logic of Date start & Date end of this Account class by
+	//       introduce parameter object -> Done
 	class Account {
-		  // ...
-		  private Vector transactions = new Vector();
-		  public Account() {
-			  transactions.add(new Transaction(1000, new Date(1000)));
-			  transactions.add(new Transaction(1200, new Date(1100)));
-			  transactions.add(new Transaction(1500, new Date(1200)));
-			  transactions.add(new Transaction(1300, new Date(1300)));
-		  }
+		// ...
+		private Vector transactions = new Vector();
+		public Account() {
+			transactions.add(new Transaction(1000, new Date(1000)));
+			transactions.add(new Transaction(1200, new Date(1100)));
+			transactions.add(new Transaction(1500, new Date(1200)));
+			transactions.add(new Transaction(1300, new Date(1300)));
+		}
 
-		  public double getFlowBetween(Date start, Date end) {
+		public double getFlowBetween(DateRange range) {
 		    double result = 0;
 		    Enumeration e = transactions.elements();
 		    while (e.hasMoreElements()) {
-		      Transaction each = (Transaction) e.nextElement();
-		      if (each.getDate().compareTo(start) >= 0 && each.getDate().compareTo(end) <= 0) {
-		        result += each.getValue();
-		      }
+		    	Transaction each = (Transaction) e.nextElement();
+		    	if (each.getDate().compareTo(range.start) >= 0 && each.getDate().compareTo(range.end) <= 0) {
+		    		result += each.getValue();
+		    	}
 		    }
 		    return result;
-		  }
 		}
+		
+	}
+    static class DateRange {
+        Date start;
+        Date end;
 
-		class Transaction {
-		  private Date chargeDate;
-		  private double value;
+        public DateRange(Date start, Date end) {
+            this.start = start;
+            this.end = end;
+        }
 
-		  public Transaction(double value, Date chargeDate) {
-		    this.value = value;
+    }
+	
+			
+	class Transaction {
+		private Date chargeDate;
+		private double value;
+
+		public Transaction(double value, Date chargeDate) {
+			this.value = value;
 		    this.chargeDate = chargeDate;
-		  }
-		  public Date getDate() {
-		    return chargeDate;
-		  }
-		  public double getValue() {
-		    return value;
-		  }
 		}
+		public Date getDate() {
+		    return chargeDate;
+		}
+		public double getValue() {
+		    return value;
+		}
+	}
 
 	private void test() {
 		Account account = new Account();
 		Date startDate = new Date(1050);
 		Date endDate = new Date(1250);
-		double flow = account.getFlowBetween(startDate, endDate);
+		double flow = account.getFlowBetween(new DateRange(startDate, endDate));
 		System.out.println(flow);
 		
 	}
